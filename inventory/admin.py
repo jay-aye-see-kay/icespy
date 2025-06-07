c4cb8238ce6a638adf7f9cd6ccc538828ab85225
Jack Rose <user@jackrose.co.nz>
2025-06-07 22:21:06 +1000

    [copilot] add models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import MealItem, MealNameSuggestion
 
 # Register your models here.
+admin.site.register(MealItem)
+admin.site.register(MealNameSuggestion)
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class MealNameSuggestion(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MealItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    meal_name = models.CharField(max_length=255)
+    portions_per_container = models.IntegerField()
+    date_added = models.DateField()
+    note = models.TextField(blank=True, null=True)
+    date_consumed = models.DateField(blank=True, null=True, default=None)
+    consumed_by_user = models.ForeignKey(
+        User, 
+        on_delete=models.SET_NULL, 
+        related_name='consumed_meals', 
+        blank=True, 
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.meal_name
