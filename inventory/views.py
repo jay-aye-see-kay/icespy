9ced18a70f41964ec784fafa84dddf8c8b196149
Jack Rose <user@jackrose.co.nz>
2025-06-07 22:29:13 +1000

    Add authentication and dashboard view for user access

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def dashboard_view(request):
+    return HttpResponse(f"Dashboard for user: {request.user.username}")
