1ebbdcd5a85f0298dcac8d95bd1e8caa6e0c5c69
Jack Rose <user@jackrose.co.nz>
2025-06-23 09:41:37 +1000

    add bootstrap

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,25 +1,29 @@
 from django import forms
 from .models import MealItem
 
-TAILWIND_FIELD_CLASSES = "w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+BOOTSTRAP_FIELD_CLASSES = "form-control"
 
 class AddMealForm(forms.Form):
     meal_name = forms.CharField(widget=forms.TextInput(attrs={
-        'class': TAILWIND_FIELD_CLASSES
+        'class': BOOTSTRAP_FIELD_CLASSES,
+        'placeholder': 'Enter meal name'
     }))
     date_added = forms.DateField(widget=forms.DateInput(attrs={
         'type': 'date',
-        'class': TAILWIND_FIELD_CLASSES
+        'class': BOOTSTRAP_FIELD_CLASSES
     }))
     number_of_containers = forms.IntegerField(min_value=1, widget=forms.NumberInput(attrs={
-        'class': TAILWIND_FIELD_CLASSES
+        'class': BOOTSTRAP_FIELD_CLASSES,
+        'placeholder': 'Number of containers'
     }))
     portions_per_container = forms.IntegerField(min_value=1, widget=forms.NumberInput(attrs={
-        'class': TAILWIND_FIELD_CLASSES
+        'class': BOOTSTRAP_FIELD_CLASSES,
+        'placeholder': 'Portions per container'
     }))
     note = forms.CharField(widget=forms.Textarea(attrs={
-        'class': TAILWIND_FIELD_CLASSES,
-        'rows': 3
+        'class': BOOTSTRAP_FIELD_CLASSES,
+        'rows': 3,
+        'placeholder': 'Optional notes'
     }), required=False)
 
 class EditMealForm(forms.ModelForm):
@@ -28,17 +32,17 @@ class EditMealForm(forms.ModelForm):
         fields = ['meal_name', 'portions_per_container', 'date_added', 'note']
         widgets = {
             'meal_name': forms.TextInput(attrs={
-                'class': TAILWIND_FIELD_CLASSES
+                'class': BOOTSTRAP_FIELD_CLASSES
             }),
             'portions_per_container': forms.NumberInput(attrs={
-                'class': TAILWIND_FIELD_CLASSES
+                'class': BOOTSTRAP_FIELD_CLASSES
             }),
             'date_added': forms.DateInput(attrs={
                 'type': 'date',
-                'class': TAILWIND_FIELD_CLASSES
+                'class': BOOTSTRAP_FIELD_CLASSES
             }),
             'note': forms.Textarea(attrs={
-                'class': TAILWIND_FIELD_CLASSES,
+                'class': BOOTSTRAP_FIELD_CLASSES,
                 'rows': 3
             }),
         }
